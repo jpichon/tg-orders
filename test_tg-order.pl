3364e2e3a171644f3b63ae005aef8b0b48027a10
Julie Pichon <julie.pichon@gmail.com>
2009-06-07 17:29:25 +0100

    Some work on Order and playing with items

--- test_tg-order.pl
+++ test_tg-order.pl
@@ -12,8 +12,28 @@ my $item2 = Item->new('Awesome tee', 1, 19.99, 'J');
 
 can_ok($item1, $_) for qw(get_name get_person get_price get_quantity);
 
-is($item1->get_name(), 'Cool new tee-shirt', 'Item->get_name');
+is($item1->get_name(), 'Cool tee-shirt', 'Item->get_name');
 is($item2->get_quantity(), 1, 'Item->get_quantity');
 is($item1->get_price(), 15.02, 'Item->get_price (1)');
 is($item2->get_price(), 19.99, 'Item->get_price (2)');
 is($item1->get_person(), 'C', 'Item->get_person');
+
+# Order
+
+my $o = Order->new("Test June '09", '52.64', '17.63');
+
+can_ok($o, $_) for qw(add_item);
+
+$o->add_item($item1);
+$o->add_item($item2);
+
+is($o->count_items, 2, 'Order->count_items');
+is($o->total_items, 35.01, 'Order->total_items');
+is($o->verify_total, 1, 'Order->verify_total');
+
+my $o2 = Order->new("Test June '09", '52.64', '17.64');
+$o2->add_item($item1);
+$o2->add_item($item2);
+
+is($o2->total_items, 35.01, 'Order->total_items (2)');
+isnt($o2->verify_total, 1, 'Order->verify_total (wrong total)');
--- tg-order.pl
+++ tg-order.pl
@@ -5,7 +5,6 @@ use strict;
 
 { package Item;
 
-
   sub new {
       my $class = shift;
       my ($name, $qty, $price, $person) = @_;
@@ -44,5 +43,80 @@ use strict;
   }
 }
 
+{ package Order;
+
+  sub new {
+      my $class = shift;
+      my $order_name = shift;
+      my $total_dollars = shift;
+      my $shipping_fees = shift;
+
+      my @items = ();
+
+      my $self = {Items => [ @items ],
+		  Title => $order_name,
+		  Shipping => $shipping_fees,
+		  Total_dollars => $total_dollars,
+		  Total_euro => 0,
+		  Customs => undef, };
+      bless $self, $class;
+
+      $self;
+  }
+
+  sub add_item {
+      my $self = shift;
+      my $item = shift;
+
+      push @{$self->{Items}}, $item;
+
+      $self->{Items};
+  }
+
+  sub count_items {
+      my $self = shift;
+
+      scalar @{$self->{Items}};
+  }
+
+  sub total_items {
+      my $self = shift;
+
+      my $total = 0;
+
+      for my $item (@{$self->{Items}}) {
+	  $total += $item->get_price();
+      }
+
+      $total;
+  }
+
+  sub verify_total {
+      my $self = shift;
+
+      (($self->total_items() + $self->{Shipping}) ==  $self->{Total_dollars});
+  }
+
+  sub print_items {
+      my $self = shift;
+
+      print $self->get_title()."\n";
+      print "Item name\tQty\tPrice\tPerson\n";
+
+      for my $item (@{$self->{Items}}) {
+	  print $item->get_name()."\t";
+	  print $item->get_quantity."\t";
+	  print $item->get_price."\t";
+	  print $item->get_person."\n";
+      }
+  }
+
+  sub get_title {
+      my $self = shift;
+
+      "ThinkGeek order ".$self->{Title}."\n";
+  }
+
+}
 
 1;
